3803da2b3271f15a64288d8ad6a1e08448edf205
Piotr Sarna <piotr@poolside.ai>
2024-08-24 18:45:31 +0200

    template + revamp

---
+++ template.sql
@@ -0,0 +1,7 @@
+insert into blog (date, title, content, link, pattern) values (
+	'2024-07-30',
+	'Simple trick to save environment and money when using GitHub Actions',
+	'The article sums up a very useful trick for GitHub actions users -- marking jobs as cancellable, so that actions are only driven to completion if they are still relevant.',
+	'https://turso.tech/blog/simple-trick-to-save-environment-and-money-when-using-github-actions',
+	'lessons learned'
+);
